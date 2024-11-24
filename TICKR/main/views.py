1aa408c359bfe3bbd3b4c0a895b1486f422051dd
sephturner <turnerj8@wit.edu>
2024-11-23 22:02:45 -0500

    General homepage layout

--- TICKR/main/views.py
+++ TICKR/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Companies
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    company_data = Companies.objects.all()
+    return render(request, 'home.html', {'company_data': company_data})
